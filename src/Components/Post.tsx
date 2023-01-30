c0b3c5099d9359f35cfa38ccd5097464e59eac10
guedesclaudio <cgsarmentosilva@gmail.com>
2023-01-30 17:07:33 -0300

    feat(timeline): created a post forms

---
+++ src/Components/Post.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Post: React.FC = () => {
+    return (
+      <Container>
+          
+      </Container>
+    );
+  };
+export default Post;
+
+const Container = styled.div``;
--- src/Pages/Timeline.tsx
+++ src/Pages/Timeline.tsx
@@ -1,6 +1,7 @@
 import TopBar from '../Components/TopBar';
 import Panel from '../Components/Panel';
 import Footer from '../Components/Footer';
+import CreatePost from '../Components/CreatePost';
 
 const Timeline: React.FC = () => {
   return (
@@ -8,7 +9,7 @@ const Timeline: React.FC = () => {
       <TopBar/>
       <>
         <Panel children={'Acompanhe os reports de outros usuários'}/>
-        
+        <CreatePost/>
         <Footer/>
       </>
     </>
